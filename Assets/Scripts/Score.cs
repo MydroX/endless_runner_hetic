3c3b11ea3aceae81e981b3faea0a8fc20cd54e7d
NatsuDzn <natsuxgraph@gmail.com>
2020-10-23 14:58:42 +0200

    feat(hud): add hud to the new game

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+  private float score = 0.0f;
+  public Text scoreText;
+
+  // Update score on every frame
+  void Update()
+  {
+    score += Time.deltaTime;
+    scoreText.text = "SCORE : " + ((int)score).ToString();
+  }
+}
